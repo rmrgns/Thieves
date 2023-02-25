2fe413fef6c175e8b2d08079557223852c1f9729
Sady <sadending82@gmail.com>
2023-02-25 15:26:47 +0900

    FBX 파일을 읽어올 수 있도록 함

--- Thieves/Engine/EnginePch.h
+++ Thieves/Engine/EnginePch.h
@@ -33,6 +33,8 @@ using namespace Microsoft::WRL;
 #include <DirectXTex/DirectXTex.h>
 #include <DirectXTex/DirectXTex.inl>
 
+#include "FBX/fbxsdk.h"
+
 
 // 각종 lib
 #pragma comment(lib, "d3d12")
@@ -44,6 +46,16 @@ using namespace Microsoft::WRL;
 #pragma comment(lib, "DirectXTex\\DirectXTex_debug.lib")
 #else
 #pragma comment(lib, "DirectXTex\\DirectXTex.lib")
+#endif
+
+#ifdef _DEBUG
+#pragma comment(lib, "FBX\\debug\\libfbxsdk-md.lib")
+#pragma comment(lib, "FBX\\debug\\libxml2-md.lib")
+#pragma comment(lib, "FBX\\debug\\zlib-md.lib")
+#else
+#pragma comment(lib, "FBX\\release\\libfbxsdk-md.lib")
+#pragma comment(lib, "FBX\\release\\libxml2-md.lib")
+#pragma comment(lib, "FBX\\release\\zlib-md.lib")
 #endif
 
 // 각종 typedef
@@ -168,4 +180,14 @@ struct TransformParams
 	Matrix matViewInv;
 };
 
+struct AnimFrameParams
+{
+	Vec4	scale;
+	Vec4	rotation; // Quaternion
+	Vec4	translation;
+};
+
 extern unique_ptr<class Engine> GEngine;
+
+wstring s2ws(const string& s);
+string ws2s(const wstring& s);
