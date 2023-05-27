cc02ae423613b03b3cb6742a54b5ff1a88a7c0f4
Lee Do Young <doyoung930@gmail.com>
2023-05-27 19:29:19 +0900

    코드수정

--- Thieves/Server/Thieves_Server/Thieves_Server/CBox.cpp
+++ Thieves/Server/Thieves_Server/Thieves_Server/CBox.cpp
@@ -28,10 +28,10 @@ bool CBox::BoxBoxIntersection(const CBox& box0)
 	C[0][2] = DotProduct(box0.axis[0],axis[2]);
 
 	AD[0] = DotProduct(box0.axis[0], D);
-	absC[0][0] = (float)fc_fabs(C[0][0]);
-	absC[0][1] = (float)fc_fabs(C[0][1]);
-	absC[0][2] = (float)fc_fabs(C[0][2]);
-	R = (float)fc_fabs(AD[0]);
+	absC[0][0] = (float)fabsf(C[0][0]);
+	absC[0][1] = (float)fabsf(C[0][1]);
+	absC[0][2] = (float)fabsf(C[0][2]);
+	R = (float)fabsf(AD[0]);
 	R1 = extent[0] * absC[0][0] + extent[1] * absC[0][1] + extent[2] * absC[0][2];
 	R01 = box0.extent[0] + R1;
 	if (R > R01)return false;
@@ -41,10 +41,10 @@ bool CBox::BoxBoxIntersection(const CBox& box0)
 	C[1][1] = DotProduct(box0.axis[1], axis[1]);
 	C[1][2] = DotProduct(box0.axis[1], axis[2]);
 	AD[1] = DotProduct(box0.axis[1], D);
-	absC[1][0] = (float)fc_fabs(C[1][0]);
-	absC[1][1] = (float)fc_fabs(C[1][1]);
-	absC[1][2] = (float)fc_fabs(C[1][2]);
-	R = (float)fc_fabs(AD[1]);
+	absC[1][0] = (float)fabsf(C[1][0]);
+	absC[1][1] = (float)fabsf(C[1][1]);
+	absC[1][2] = (float)fabsf(C[1][2]);
+	R = (float)fabsf(AD[1]);
 	R1 = extent[0] * absC[1][0] + extent[1] * absC[1][1] + extent[2] * absC[1][2];
 	R01 = box0.extent[1] + R1;
 	if (R > R01)return false;
@@ -54,10 +54,10 @@ bool CBox::BoxBoxIntersection(const CBox& box0)
 	C[2][1] = DotProduct(box0.axis[2], axis[1]);
 	C[2][2] = DotProduct(box0.axis[2], axis[2]);
 	AD[2] = DotProduct(box0.axis[2], D);
-	absC[2][0] = (float)fc_fabs(C[2][0]);
-	absC[2][1] = (float)fc_fabs(C[2][1]);
-	absC[2][2] = (float)fc_fabs(C[2][2]);
-	R = (float)fc_fabs(AD[2]);
+	absC[2][0] = (float)fabsf(C[2][0]);
+	absC[2][1] = (float)fabsf(C[2][1]);
+	absC[2][2] = (float)fabsf(C[2][2]);
+	R = (float)fabsf(AD[2]);
 	R1 = extent[0] * absC[2][0] + extent[1] * absC[2][1] + extent[2] * absC[2][2];
 	R01 = box0.extent[2] + R1;
 	if (R > R01)return false;
@@ -65,19 +65,19 @@ bool CBox::BoxBoxIntersection(const CBox& box0)
 
 	// axis 축과의 충돌체크
 	//B0
-	R = (float)fc_fabs(DotProduct(axis[0], D));
+	R = (float)fabsf(DotProduct(axis[0], D));
 	R0 = box0.extent[0] * absC[0][0] + box0.extent[1] * absC[1][0] + box0.extent[2] * absC[2][0];
 	R01 = R0 + extent[0];
 	if (R > R01)return false;
 
 	//B1
-	R = (float)fc_fabs(DotProduct(axis[1], D));
+	R = (float)fabsf(DotProduct(axis[1], D));
 	R0 = box0.extent[0] * absC[0][1] + box0.extent[1] * absC[1][1] + box0.extent[2] * absC[2][1];
 	R01 = R0 + extent[1];
 	if (R > R01)return false;
 
 	//B2
-	R = (float)fc_fabs(DotProduct(axis[2], D));
+	R = (float)fabsf(DotProduct(axis[2], D));
 	R0 = box0.extent[0] * absC[0][2] + box0.extent[1] * absC[1][2] + box0.extent[2] * absC[2][2];
 	R01 = R0 + extent[2];
 	if (R > R01)return false;
@@ -85,63 +85,63 @@ bool CBox::BoxBoxIntersection(const CBox& box0)
 
 	//axis 벡터들 간의 외적과 내적을 사용하여 충돌 체크
 	//A0xB0
-	R = (float)fc_fabs(AD[2] * C[1][0] - AD[1] * C[2][0]);
+	R = (float)fabsf(AD[2] * C[1][0] - AD[1] * C[2][0]);
 	R0 = box0.extent[1] * absC[2][0] + box0.extent[2] * absC[1][0];
 	R1 = extent[1] * absC[0][2] + extent[2] * absC[0][1];
 	R01 = R0 + R1;
 	if (R > R01)return false;
 
 	//A0xB1
-	R = (float)fc_fabs(AD[2] * C[1][1] - AD[1] * C[2][1]);
+	R = (float)fabsf(AD[2] * C[1][1] - AD[1] * C[2][1]);
 	R0 = box0.extent[1] * absC[2][1] + box0.extent[2] * absC[1][1];
 	R1 = extent[0] * absC[0][2] + extent[2] * absC[0][0];
 	R01 = R0 + R1;
 	if (R > R01)return false;
 
 	//A0xB2
-	R = (float)fc_fabs(AD[2] * C[1][2] - AD[1] * C[2][2]);
+	R = (float)fabsf(AD[2] * C[1][2] - AD[1] * C[2][2]);
 	R0 = box0.extent[1] * absC[2][2] + box0.extent[2] * absC[1][2];
 	R1 = extent[0] * absC[0][1] + extent[1] * absC[0][0];
 	R01 = R0 + R1;
 	if (R > R01)return false;
 
 	//A1xB0
-	R = (float)fc_fabs(AD[0] * C[2][0] - AD[2] * C[0][0]);
+	R = (float)fabsf(AD[0] * C[2][0] - AD[2] * C[0][0]);
 	R0 = box0.extent[0] * absC[2][0] + box0.extent[2] * absC[0][0];
 	R1 = extent[1] * absC[1][2] + extent[2] * absC[1][1];
 	R01 = R0 + R1;
 	if (R > R01)return false;
 
 	//A1xB1
-	R = (float)fc_fabs(AD[0] * C[2][1] - AD[2] * C[0][1]);
+	R = (float)fabsf(AD[0] * C[2][1] - AD[2] * C[0][1]);
 	R0 = box0.extent[0] * absC[2][1] + box0.extent[2] * absC[0][1];
 	R1 = extent[0] * absC[1][2] + extent[2] * absC[1][0];
 	R01 = R0 + R1;
 	if (R > R01)return false;
 
 	//A1xB2
-	R = (float)fc_fabs(AD[0] * C[2][2] - AD[2] * C[0][2]);
+	R = (float)fabsf(AD[0] * C[2][2] - AD[2] * C[0][2]);
 	R0 = box0.extent[0] * absC[2][2] + box0.extent[2] * absC[0][2];
 	R1 = extent[0] * absC[1][1] + extent[1] * absC[1][0];
 	R01 = R0 + R1;
 	if (R > R01)return false;
 
 	//A2xB0
-	R = (float)fc_fabs(AD[1] * C[0][0] - AD[0] * C[1][0]);
+	R = (float)fabsf(AD[1] * C[0][0] - AD[0] * C[1][0]);
 	R0 = box0.extent[0] * absC[1][0] + box0.extent[1] * absC[0][0];
 	R1 = extent[1] * absC[2][2] + extent[2] * absC[2][1];
 	R01 = R0 + R1;
 	if (R > R01)return false;
 
 	//A2xB1
-	R = (float)fc_fabs(AD[1] * C[0][1] - AD[0] * C[1][1]);
+	R = (float)fabsf(AD[1] * C[0][1] - AD[0] * C[1][1]);
 	R0 = box0.extent[0] * absC[1][1] + box0.extent[1] * absC[0][1];
 	R1 = extent[0] * absC[2][2] + extent[2] * absC[2][0];
 	R01 = R0 + R1;
 	if (R > R01)return false;
 
 	//A2xB2
-	R = (float)fc_fabs(AD[1] * C[0][2] - AD[0] * C[1][2]);
+	R = (float)fabsf(AD[1] * C[0][2] - AD[0] * C[1][2]);
 	R0 = box0.extent[0] * absC[1][2] + box0.extent[1] * absC[0][2];
 	R1 = extent[0] * absC[2][1] + extent[1] * absC[2][0];
 	R01 = R0 + R1;
--- Thieves/Server/Thieves_Server/Thieves_Server/CBox.h
+++ Thieves/Server/Thieves_Server/Thieves_Server/CBox.h
@@ -44,5 +44,5 @@ inline float DotProduct(const float v0[3], const float v1[3])
 	return v0[0] * v1[0] + v0[1] * v1[1] + v0[2] * v1[2];
 }
 
-#define fc_fabs fabsf
+//#define fc_fabs fabsf
 
