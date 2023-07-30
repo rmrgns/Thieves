1a3eeb4b68439cb2160121251e222c41002c3e72
Jung Geuk Hun <111038432+rmrgns@users.noreply.github.com>
2023-07-30 17:49:01 +0900

    Update LightEffect.cpp
    페이즈전환 서버코드 추가

--- Thieves/Engine/LightEffect.cpp
+++ Thieves/Engine/LightEffect.cpp
@@ -5,6 +5,12 @@
 #include "Scene.h"
 #include "Light.h"
 #include "Input.h"
+#include "InGameScene.h"
+
+#include "server/main/network.h"
+#include "server/thieves_server/thieves_packet_manager.h"
+#include "server/main/network_move_object.h"
+#include "server/thieves_server/game_info.h"
 
 LightEffect::LightEffect()
 {
@@ -16,7 +22,38 @@ LightEffect::~LightEffect()
 
 void LightEffect::LateUpdate()
 {
+	if (_lightState == true)
+	{
+		auto iScene = static_pointer_cast<InGameScene>(GET_SINGLE(SceneManager)->GetActiveScene());
 
+		if (iScene->GetPhaseChanged())
+		{
+			// 2 ÆäÀÌÁî
+
+			if (this->GetGameObject()->GetName() == L"Dir_Light")
+			{
+
+				//this->GetGameObject()->GetLight()->SetLightState(false);
+
+				this->GetGameObject()->GetLight()->SetDiffuse(Vec3(0.8f, 0.8f, 0.8f));
+				this->GetGameObject()->GetLight()->SetAmbient(Vec3(0.5f, 0.5f, 0.5f));
+				this->GetGameObject()->GetLight()->SetSpecular(Vec3(0.5f, 0.5f, 0.5f));
+				_lightState = false;
+
+			}
+			if (this->GetGameObject()->GetName() == L"DiaRoom_Pnt_Light")
+			{
+
+				//this->GetGameObject()->GetLight()->SetLightState(false);
+				this->GetGameObject()->GetLight()->SetDiffuse(Vec3(0.f, 0.f, 0.f));
+				this->GetGameObject()->GetLight()->SetAmbient(Vec3(0.f, 0.f, 0.f));
+				this->GetGameObject()->GetLight()->SetSpecular(Vec3(0.f, 0.f, 0.f));
+				_lightState = false;
+
+			}
+		}
+	}
+	
 	//if (this->GetGameObject()->GetName() == L"Dir_Light")
 	//{
 	//	if (_lightState == true)
